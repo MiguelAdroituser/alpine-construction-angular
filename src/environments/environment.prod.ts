c27d91afd9b84eb717dcafb16b4212df40fe5737
MiguelAdroituser <miguel@adroitoverseas.com>
2026-05-09 19:46:01 -0700

    Feat - test url seenode.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3001/',
+    // apiUrl: 'http://localhost:3001/',
+    apiUrl: 'https://web-6hkep36q5tbp.up-de-fra1-k8s-1.apps.run-on-seenode.com/',
     secretKey: 'TU_CLAVE_SECRETA',
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3001/api/',
+    // apiUrl: 'http://localhost:3001/api/',
+    apiUrl: 'https://web-6hkep36q5tbp.up-de-fra1-k8s-1.apps.run-on-seenode.com/api/',
   };
